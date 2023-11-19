f83eae10adec9473ec0f1deb8c94d41828d6c166
Atatsuki2000 <sammylee20001021@gmail.com>
2023-11-18 16:54:55 -0800

    Modified preprocessData.py and truncateData.py

--- preprocessData.py
+++ preprocessData.py
@@ -1,26 +1,34 @@
 import argparse
 import string
 
-def preprocessData(input_path, output_path):
+def preprocessData(file_path, line_length=64):
     # Open the input file for reading
-    with open(input_path, 'r') as input_file:
+    with open(file_path, 'r') as input_file:
         # Open a new file for writing the filtered content
-        with open(output_path, 'w') as output_file:
+        with open(f'{file_path}_trimmed{line_length}', 'w') as output_file:
             # Read and process each line in the input file
             for line in input_file:
                 if line == '\n':
                     continue
                 # Make characters to lowercases
                 line = line.lower()
-                # Strip punctuations
+                # Strip punctuations and leading/trailing whitespaces
                 translation_table = str.maketrans("", "", string.punctuation)
                 stripped_line = line.translate(translation_table).strip()
                 # Change whitespace to underscore
-                processed_line = stripped_line.replace(' ', '_')
-                # Separacter characters with whitespace
-                output_line = ' '.join(processed_line) + '\n'
-                # Write line to file
-                output_file.write(output_line)
+                line_with_underscores = stripped_line.replace(' ', '_')
+
+                # Check if the line with underscores is less than or equal to 64 characters
+                if len(line_with_underscores) <= line_length:
+                    # If the modified line is within the desired length, write it to the output file
+                    output_line = ' '.join(line_with_underscores) + '\n'
+                    # Write line to file
+                    output_file.write(processed_line)
+                else:
+                    # If the modified line is longer, trim it to the desired line length and then write it
+                    processed_line = ' '.join(line_with_underscores[:line_length]) + '\n'
+                    # Write line to file
+                    output_file.write(processed_line)
 
 def main():
     parser = argparse.ArgumentParser(description='Preprocess data by modifying data into lowercase, stripping punctuation, and changing whitespaces into underscores')
